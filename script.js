4c325bb174dcfd39093ca27e7e06a25994fc8662
TenCreator <45293208+tencreator@users.noreply.github.com>
2023-02-07 20:59:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const progressBar = document.getElementsByClassName('progress-bar')[0]
 let progress = 0
 setInterval(() => {
+  if (progress >= 1000) return
   const computedStyle = getComputedStyle(progressBar)
   const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
   progressBar.style.setProperty('--width', width + .1)
   progress ++
-  console.log(progress)
+  if (progress =< 800) progredsBar.innerHTML;
 }, 5)
